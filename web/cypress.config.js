8e31386f092bc20228fa98660e5915b2d3bdcd12
mcello23 <marceloadsc@gmail.com>
2025-06-07 17:34:38 +0200

    adding yml

--- web/cypress.config.js
+++ web/cypress.config.js
@@ -1,6 +1,8 @@
 export default {
   e2e: {
     projectId: 'bv1wq1',
+    enableScreenshots: true,
+    enableVideo: true,
     baseUrl: 'http://localhost:3000',
     viewportWidth: 1920,
     viewportHeight: 1080,

e5848d6ae7953993c39695a12c330fa195129bbd
Marcelo <marceloadsc@gmail.com>
2025-09-24 06:41:56 +0200

    Updating test with cy.prompt

--- web/cypress/e2e/login.cy.js
+++ web/cypress/e2e/login.cy.js
@@ -26,7 +26,32 @@ describe('Login', () => {
     );
   });
 
-  it.skip('Uses cy.prompt', () => {
-    cy.prompt(['Visit https://cloud.cypress.io/login']);
+  it('Uses cy.prompt', () => {
+    cy.prompt([
+      'Insert email marcelo@webdojo.com into the the input e-mail',
+      'Insert password katana123 into the input password',
+      'Validate that both fields were filled correctly',
+      'Validate that the Entrar button is enabled',
+      'Click on the Entrar button',
+      'Validate that the user name is Marcelo Costa',
+    ]);
+    // // Prompt step 1: Insert email marcelo@webdojo.com into the the input e-mail
+    // cy.get("#email").type("marcelo@webdojo.com");
+
+    // // Prompt step 2: Insert password katana123 into the input password
+    // cy.get("#password").type("katana123")
+
+    // // Prompt step 3: Validate that both fields were filled correctly
+    // cy.get("#email").should("have.value", "marcelo@webdojo.com")
+    // cy.get("#password").should("have.value", "katana123")
+
+    // // Prompt step 4: Validate that the Entrar button is enabled
+    // cy.get("#root .text-white").should("be.enabled")
+
+    // // Prompt step 5: Click on the Entrar button
+    // cy.get("#root .text-white").click()
+
+    // // Prompt step 6: Validate that the user name is Marcelo Costa
+    // cy.get("[data-cy=\"user-name\"]").should("have.text", "Marcelo Costa")
   });
 });
